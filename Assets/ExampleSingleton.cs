4648f977f6bf7e0fd5f7d28e224d6ee69c9f22e6
Simon Heinen <simon.heinen@gmail.com>
2017-05-28 12:46:31 +0200

    singleton example

---
+++ Assets/ExampleSingleton.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class ExampleSingleton : MonoBehaviour {
+    public static ExampleSingleton instance { get { return Singleton.get<ExampleSingleton>(); } }
+    public string myVar1 = "abc";
+    public void Start() { Assert.AreEqual(this, instance, "Singleton more than once in scene"); } 
+}
+
+/// <summary> Helper that turns any MonBehaviour or other Component into a Singleton </summary>
+public static class Singleton {
+    public static T get<T>() where T : Component {
+        return GetOrAddGo("Singletons").GetOrAddChild("" + typeof(T)).GetOrAddComponent<T>();
+    }
+    private static GameObject GetOrAddGo(string goName) {
+        var go = GameObject.Find(goName);
+        if (go == null) { return new GameObject(goName); }
+        return go;
+    }
+}
+
+public static class GameObjectExtensionMethods { 
+    public static GameObject GetOrAddChild(this GameObject parentGo, string childName) {
+        var childGo = parentGo.transform.FindChild(childName);
+        if (childGo != null) { return childGo.gameObject; } // child found, return it
+        var newChild = new GameObject(childName);        // no child found, create it
+        newChild.transform.SetParent(parentGo.transform, false); // add it to parent
+        return newChild;
+    }
+
+    public static T GetOrAddComponent<T>(this GameObject parentGo) where T : Component {
+        var comp = parentGo.GetComponent<T>();
+        if (comp == null) { return parentGo.AddComponent<T>(); }
+        return comp;
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Test : MonoBehaviour {
+    void Start() {
+        ExampleSingleton singleton = ExampleSingleton.instance;
+        Assert.IsNotNull(singleton); // automatic initialization on first usage
+        Assert.AreEqual("abc", singleton.myVar1);
+        singleton.myVar1 = "123";
+        // multiple calls to instance() return the same object:
+        Assert.AreEqual(singleton, ExampleSingleton.instance); 
+        Assert.AreEqual("123", ExampleSingleton.instance.myVar1);
+    }
+}
