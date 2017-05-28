6a551ea3043deb89717474aebbc754fd755ea86c
Simon Heinen <simon.heinen@gmail.com>
2017-05-28 13:57:47 +0200

    added param to not create a singleton if not already extant

--- Assets/ExampleSingleton.cs
+++ Assets/ExampleSingleton.cs
@@ -4,13 +4,15 @@ using UnityEngine.Assertions;
 public class ExampleSingleton : MonoBehaviour {
     public static ExampleSingleton instance { get { return Singleton.get<ExampleSingleton>(); } }
     public string myVar1 = "abc";
-    public void Start() { Assert.AreEqual(this, instance, "Singleton more than once in scene"); } 
+    public void Start() { Assert.AreEqual(this, instance, "Singleton more than once in scene"); }
 }
 
 /// <summary> Helper that turns any MonBehaviour or other Component into a Singleton </summary>
 public static class Singleton {
-    public static T get<T>() where T : Component {
-        return GetOrAddGo("Singletons").GetOrAddChild("" + typeof(T)).GetOrAddComponent<T>();
+    public static T get<T>(bool createIfNull = true) where T : Component {
+        if (createIfNull) { return GetOrAddGo("Singletons").GetOrAddChild("" + typeof(T)).GetOrAddComponent<T>(); }
+        var t = GetOrAddGo("Singletons").transform.FindChild("" + typeof(T));
+        return t != null ? t.GetComponent<T>() : null;
     }
     private static GameObject GetOrAddGo(string goName) {
         var go = GameObject.Find(goName);
@@ -19,7 +21,7 @@ public static class Singleton {
     }
 }
 
-public static class GameObjectExtensionMethods { 
+public static class GameObjectExtensionMethods {
     public static GameObject GetOrAddChild(this GameObject parentGo, string childName) {
         var childGo = parentGo.transform.FindChild(childName);
         if (childGo != null) { return childGo.gameObject; } // child found, return it
